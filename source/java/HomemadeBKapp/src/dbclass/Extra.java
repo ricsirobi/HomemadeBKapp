ad718a87eafb952383066cfd8916c46174654938
ricsirobi <acs.robert2000@gmail.com>
2022-05-22 02:00:36 +0200

    created extra class

---
+++ source/java/HomemadeBKapp/src/dbclass/Extra.java
@@ -0,0 +1,18 @@
+package dbclass;
+
+public class Extra {
+    private int id;
+    private String name;
+    private int count;
+    private int price;
+
+    public int getId() {return id;}
+    public String getName() {return name;}
+    public int getCount() {return count;}
+    public int getPrice() {return price;}
+
+    public void setId(int id) {this.id=id;}
+    public void setName(String name) {this.name=name;}
+    public void setCount(int count) {this.count=count;}
+    public void setPrice(int price) {this.price=price;}
+}
