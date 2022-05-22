c03b26e5b328ff381c701d5fc5d93c28f7a3b9dc
ricsirobi <acs.robert2000@gmail.com>
2022-05-22 02:00:27 +0200

    created menu class

---
+++ source/java/HomemadeBKapp/src/dbclass/Menu.java
@@ -0,0 +1,18 @@
+package dbclass;
+
+public class Menu {
+    private int id;
+    private int burgerid;
+    private int drinkid;
+    private int userid;
+
+    public int getId() {return id;}
+    public int getBurgerid() {return burgerid;}
+    public int getDrinkid() {return drinkid;}
+    public int getUserid() {return userid;}
+
+    public void setId(int id) {this.id=id;}
+    public void setBurgerid(int id) {this.burgerid=id;}
+    public void setDrinkid(int id) {this.drinkid=id;}
+    public void setUserid(int id) {this.userid=id;}
+}
